5a7fb705bba0ee2119ba59ee495ea8f573ac8555
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2024-08-16 22:55:17 +0530

    first commi

--- app/components/Pricing.jsx
+++ app/components/Pricing.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { FaCheckCircle } from "react-icons/fa";
+
 import Link from 'next/link'
 import BuyProduct from "./razorpay/BuyProduct";
 const Pricing = () => {
